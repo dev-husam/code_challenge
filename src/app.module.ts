0ec50d429921ec8bf4c91ab226fce571c2fa8194
hussam dev <dev.h.maher@gmail.com>
2024-11-27 09:05:26 +0300

    add config service

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,21 +5,25 @@ import { SequelizeModule } from '@nestjs/sequelize';
 
 import { ProductsModule } from './products/products.module';
 import { Product } from './products/entities/product.entity';
+import { ConfigModule } from '@nestjs/config';
+
+console.log({ sd: process.env.DB_HOST });
 
 @Module({
-  imports: [ProductsModule,
-    SequelizeModule.forRoot({
-      dialect: 'mysql',
-      host: 'db',
-      port: 3306,
-      username: 'root',
-      password: 'password',
-      database: 'task1',
-      models: [Product],
-      autoLoadModels: true,
-      synchronize: true
+  imports: [ConfigModule.forRoot({ isGlobal: true, envFilePath: ".env" }),
+    ProductsModule,
+  SequelizeModule.forRoot({
+    dialect: 'mysql',
+    host: process.env.DB_HOST,
+    port: 3306,
+    username: 'root',
+    password: 'password',
+    database: 'task1',
+    models: [Product],
+    autoLoadModels: true,
+    synchronize: true
 
-    }),
+  }),
   ],
   controllers: [AppController],
   providers: [AppService],
--- src/products/dto/create-product.dto.ts
+++ src/products/dto/create-product.dto.ts
@@ -1,9 +1,17 @@
-export class CreateProductDto {
+import { IsBoolean, IsNumber, IsOptional, IsString } from "class-validator"
 
+export class CreateProductDto {
+    @IsString()
     name: string
 
+    @IsString()
     descriptions: string
 
+    @IsNumber()
     qty: number
+
+    @IsOptional()
+    @IsBoolean()
+    isActive: number
 
 }
